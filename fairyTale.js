dfaf1051924ec01314608861026861eda70927c8
Skraftz <davekenkent@gmail.com>
2024-06-11 10:58:49 -0400

    toasr

--- fairyTale.js
+++ fairyTale.js
@@ -1,29 +1,29 @@
 // 1. Create a variable named `place` and set the value to a string that says whatever place you want.
-
+let place = "John Island";
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
-
+let creatureName = "John Creature OwO";
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
-
+let color = "#a14fb6";
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
-
+let color2 = "#5d34aa";
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
-
+let emotion = "like 10 years into the joint made you a fu-. They had enough";
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
-
+let character = "John Yakuza";
 // 7. Create a variable named `number` and set the value to a number greater than 1.
-
+let number = 2;
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
-
+let item = "John Metal Pipe";
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
-
+let item2 = "John Survival";
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
-
+let verb = "kill";
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
-
+let adverb = "John";
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
+let number2 = 621;
 
-
-
+let madlyb = "#d9ead3";
 
 
 
@@ -31,19 +31,33 @@
 // - Afterwards, run your code and press the button to see your story ✨
 
 document.querySelector("button").addEventListener("click", function() {
-    document.querySelector(".place").innerHTML = null;
-    document.querySelector(".creature-name").innerHTML = null;
-    document.querySelector(".color").innerHTML = null;
-    document.querySelector(".color-2").innerHTML = null;
-    document.querySelector(".emotion").innerHTML = null;
-    document.querySelector(".character").innerHTML = null;
-    document.querySelector(".creature-name-again").innerHTML = null;
-    document.querySelector(".number").innerHTML = null;
-    document.querySelector(".item").innerHTML = null;
-    document.querySelector(".character-again").innerHTML = null;
-    document.querySelector(".item-2").innerHTML = null;
-    document.querySelector(".verb").innerHTML = null;
-    document.querySelector(".adverb").innerHTML = null;
-    document.querySelector(".number-2").innerHTML = null;
+    document.querySelector(".place").innerHTML = place;
+    document.querySelector(".place").style.color = madlyb;
+    document.querySelector(".creature-name").innerHTML = creatureName;
+    document.querySelector(".creature-name").style.color = madlyb;
+    document.querySelector(".color").innerHTML = color;
+    document.querySelector(".color").style.color = color;
+    document.querySelector(".color-2").innerHTML = color2;
+    document.querySelector(".color-2").style.color = color2;
+    document.querySelector(".emotion").innerHTML = emotion;
+    document.querySelector(".emotion").style.color = madlyb
+    document.querySelector(".character").innerHTML = character;
+    document.querySelector(".character").style.color = madlyb
+    document.querySelector(".creature-name-again").style.color = madlyb
+    document.querySelector(".number").style.color = madlyb
+    document.querySelector(".item").style.color = madlyb
+    document.querySelector(".character-again").style.color = madlyb
+    document.querySelector(".item-2").style.color = madlyb
+    document.querySelector(".verb").style.color = madlyb
+    document.querySelector(".adverb").style.color = madlyb
+    document.querySelector(".number-2").style.color = madlyb
+    document.querySelector(".creature-name-again").innerHTML = creatureName;
+    document.querySelector(".number").innerHTML = number;
+    document.querySelector(".item").innerHTML = item;
+    document.querySelector(".character-again").innerHTML = character;
+    document.querySelector(".item-2").innerHTML = item2;
+    document.querySelector(".verb").innerHTML = verb;
+    document.querySelector(".adverb").innerHTML = adverb;
+    document.querySelector(".number-2").innerHTML = number2;
     document.querySelector(".story-container").style = "display:block";
 });
--- variables.js
+++ variables.js
@@ -2,10 +2,10 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
@@ -14,5 +14,16 @@ let favoriteColor;
 // - Use a string value of your favorite color.
 // - Use a string value of your favorite food.
 // - Use a number value of your favorite number.
-
+favoriteColor = "Orange"; //Sets it to "orange"
+favoriteFood = favoriteColor; //set's it to "orange"
 // 4. Console log each variable on a new line.
+function Imcoughingreallybad () { //Inistiates and defines the function
+    favoriteNumber = Math.random(); //Changes the favorite number to a random number
+    console.log("My favorite number is always changing, but right now it is", favoriteNumber,"."); // Prints the favorite number of that second
+    setTimeout(Imcoughingreallybad, 5000); //Runs the function Imcoughingreallybad()
+};
+
+
+console.log("My favorite color is ", favoriteFood,".");
+console.log("My favorite food is ", favoriteColor,".");
+Imcoughingreallybad();
